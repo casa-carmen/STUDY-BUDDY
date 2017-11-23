70fd1925abc41efeb56a9feead24995c098e4bb2
Carmen <zcbtcfi@ucl.ac.uk>
2017-11-23 20:03:31 +0000

    changed

--- script/coolscript.js
+++ script/coolscript.js
@@ -2,13 +2,13 @@ $(document).ready(function()
 {
 	$(".things").hide();
 
-function Find your fit(){
+function Find_your_fit(){
 
 
-	$("#Find your fit").show();
+	$("#Find_your_fit").show();
 	$("#Social").hide();	
 	$("#Location").hide();	
-	$(".Find your fit").css("background-color", 'gold');
+	$(".Find_your_fit").css("background-color", 'gold');
 	$(".Social").css("background-color", '#868e96');
 	$(".Location").css("background-color", '#868e96');
 } 
@@ -17,11 +17,11 @@ function Find your fit(){
 function Location()
 {
 	$("#Location").show();
-	$("#Find your fit").hide();
+	$("#Find_your_fit").hide();
 	$("#Social").hide();
 	$(".Location").css("background-color", 'gold');
 	$(".Hugo").css("background-color", '#868e96');
-	$(".Find your fit").css("background-color", '#868e96');
+	$(".Find_your_fit").css("background-color", '#868e96');
 }
 
 
@@ -31,9 +31,9 @@ function Social(){
 
 	$("#Social").show();	
 	$("#Location").hide();
-	$("#Find your fit").hide();
+	$("#Find_your_fit").hide();
 	$(".Social").css("background-color", 'gold');
-	$(".Find your fit").css("background-color", '#868e96');
+	$(".Find_your_fit").css("background-color", '#868e96');
 	$(".Location").css("background-color", '#868e96');
 } 
 
@@ -41,7 +41,7 @@ function Social(){
 
 	
 	$(".Location").click(Location);
-	$(".Find your fit").click(Find your fit);
+	$(".Find_your_fit").click(Find_your_fit);
 	$(".Social").click(Social);
 
 });
